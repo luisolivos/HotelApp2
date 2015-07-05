88359806879e3eac1597ad2515b8ec41fe989387
luisolivos <pp_its@hotmail.com>
2015-07-05 00:29:17 -0500

    Update mediaCapture.js

--- js/API/mediaCapture.js
+++ js/API/mediaCapture.js
@@ -1,12 +1,13 @@
 var mc{
-    // capture callback
+     // capture callback
     captureSuccess: function(mediaFiles) {
         var i, path, len;
         for (i = 0, len = mediaFiles.length; i < len; i += 1) {
             path = mediaFiles[i].fullPath;
-            $("#fotoTomada").html('<img src="' + path +'" width="100%"'>);
+            $("#fotoTomada").html('<img src="' + path +'" width="100%">');
             $("#fototomada").attr("rel", path);
         }
+       // navigator.notification.alert('Error code: OK', null, 'Capture Error');
     },
 
     // capture error callback
