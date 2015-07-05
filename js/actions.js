cdd093bb9a72229d87515ce4a16f04fbfae04a7a
luisolivos <pp_its@hotmail.com>
2015-07-05 00:23:08 -0500

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -51,7 +51,7 @@ var fn = {
         var i, path, len;
         for (i = 0, len = mediaFiles.length; i < len; i += 1) {
             path = mediaFiles[i].fullPath;
-        //    $("#fotoTomada").html('<img src="' + path +'" width="100%"'>);
+            $("#fotoTomada").html('<img src="' + path +'" width="100%"'>);
         //    $("#fototomada").attr("rel", path);
         }
         navigator.notification.alert('Error code: OK', null, 'Capture Error');
